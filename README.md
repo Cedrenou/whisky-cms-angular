0aaddf5e57a7fb34895279f3b961c452e6e2b1a5
Cedrenou <ced2411@gmail.com>
2019-05-15 14:46:22 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to start the server side
+
+1. In the first tab do : `mongod --config /usr/local/etc/mongod.conf`
+2. In the second tab start mongo with : `mongo`
+3. Finally start the server with `npm start`
---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const api = require('./api/v1/index')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+
+const mongoose = require('mongoose')
+const connection = mongoose.connection
+
+app.set('port', (process.env.port || 3000))
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({
+	extended: false
+}))
+app.use(cors())
+app.use('/api/v1', api)
+
+app.use((req, res) => {
+	const err = new Error('404 - not found !!!')
+	err.status = 404
+	res.json({
+		msg: '404 - not found !!!',
+		err: err
+	})
+})
+
+mongoose.connect('mongodb://localhost:27017/whiskycms', {
+	useNewUrlParser: true
+})
+connection.on('error', (err) => {
+	console.error(`connection to MongoDB error: ${err.message}`)
+})
+
+connection.once('open', () => {
+	console.log('Connected to MongoDB')
+
+	app.listen(app.get('port'), () => {
+		console.log(`express-server listening on port ${app.get('port')}`)
+	})
+})
