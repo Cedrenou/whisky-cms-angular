6ec3f35f5df84c0b84ac7ab7073fdf6ce4cade7b
Cedrenou <ced2411@gmail.com>
2019-05-17 16:24:57 +0200

    CRUD method and API ready

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const Blogpost = require('../models/blogpost')
+const mongoose = require('mongoose')
+const multer = require('multer')
+const crypto = require('crypto')
+const path = require('path')
 
 router.get('/ping', (req, res) => {
 	res.status(200).json({msg: 'pong', date: new Date().toLocaleString()})
 })
 
 router.get('/blog-posts', (req, res) => {
-	find()
+	Blogpost.find()
 		.sort({'createdOn': -1})
 		.exec()
 		.then(blogPosts => res.status(200).json(blogPosts))
@@ -17,6 +21,25 @@ router.get('/blog-posts', (req, res) => {
 		}))
 })
 
+// File upload config
+const storage = multer.diskStorage({
+	destination: './uploads/',
+	filename: function(req, file, callback) {
+		crypto.pseudoRandomBytes(16, (err, raw) => {
+			if(err) return
+			callback(null, raw.toString('hex') + path.extname(file.originalname))
+		})
+	}
+})
+const upload = multer({storage: storage})
+
+// file upload
+router.post('/blog-posts/images', upload.single('blogimage'), (req,res) => {
+	if(!req.file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+		res.status(400).json({msg: 'only image files please !'})
+	}
+	res.status(201).send({filename: req.file.filename, file: req.file})
+})
 
 // CREATE
 router.post('/blog-posts', (req, res) => {
@@ -34,7 +57,7 @@ router.post('/blog-posts', (req, res) => {
 // READ
 router.get('/blog-posts/:id', (req, res) => {
 	const id = req.params.id
-	findById(id)
+	Blogpost.findById(id)
 		.then(blogPost => res.status(200).json(blogPost))
 		.catch(err => res.status(500).json({
 			message: `blog post with id ${id} not found`,
@@ -45,11 +68,30 @@ router.get('/blog-posts/:id', (req, res) => {
 // DELETE
 router.delete('/blog-posts/:id', (req, res) => {
 	const id = req.params.id
-	findByIdAndDelete(id, (err, blogPost) => {
+	Blogpost.findByIdAndDelete(id, (err, blogPost) => {
 		if (err) {
 			return res.status(500).json(err)
 		}
 		res.status(202).json({msg: `blog post with id ${blogPost._id} deleted !`})
+	})
+})
+
+router.delete('/blog-posts', (req,res) => {
+	const ids = req.query.ids
+	console.log(ids)
+	const allIds = ids.split(',').map(id => {
+		if(id.match(/^[0-9a-fA-F]{24}$/)) {
+			return mongoose.Types.ObjectId(id)
+		} else {
+			console.log('Id is not valid', id)
+		}
+	})
+	const condition = {_id: {$in: allIds}}
+	Blogpost.deleteMany(condition, (err, result) => {
+		if (err) {
+			return res.status(500).json(err)
+		} 
+		res.status(202).json(result)
 	})
 })
 
